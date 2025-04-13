ffc4a066d102d511e8bddb8065e148fa934aadf9
rafavoa <rafaela.araujo@voa.delivery>
2025-04-13 15:23:15 -0300

    criando o repositório

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Home from "./components/home.vue";
+</script>
+
+<template>
+
+    <home></home>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class=" flex justify-center items-center w-full h-screen">
+  <div class=" text-6xl "> estruturas matemáticas</div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
