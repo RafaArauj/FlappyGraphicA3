37e2a03db96ea36b041af5b4c67e05a9dff87186
rafavoa <rafaela.araujo@voa.delivery>
2025-05-03 21:33:11 -0300

    centralizando as coisas

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-4">
-    <h1 class="text-2xl font-bold mb-4">Flappy Graphic</h1>
+    <h1 class="text-2xl flex justify-center font-bold mb-4">Flappy Graphic</h1>
     <PipeGame />
   </div>
 </template>
--- src/components/PipeGame.vue
+++ src/components/PipeGame.vue
@@ -16,7 +16,7 @@
 </script>
 
 <template>
-  <div>
+  <div class="flex flex-col justify-center items-center">
     <FormulaInput v-model="formula" />
     <GraphCanvas :formula="formula" :pipes="pipes" />
   </div>
