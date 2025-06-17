356c62dfa0f42bd504c66fd7cc8d5b80c0947047
jpnunes012 <jpjoaopedronunes@gmail.com>
2025-06-17 16:54:10 -0300

    editando o texto do how-play

--- src/components/how-play.vue
+++ src/components/how-play.vue
@@ -7,10 +7,22 @@ import TextStructure from "@/components/text-structure.vue";
   <div class="flex flex-col justify-center items-start">
 <span class="text-2xl self-center pt-4">Como Jogar</span>
     <div class=" flex flex-col w-[500px] self-center pt-4 gap-y-2 text-justify">
-<text-structure title="1." content="Use sempre a variável x
+<text-structure title="1." content=" A linha será desenhada automaticamente no plano cartesiano
+
+Assim que você escrever a função, a linha aparecerá no gráfico. O desafio é fazer essa linha passar entre os espaços dos obstáculos.
+
 → Sua função deve ser escrita em termos de x.  Exemplo:2*x-4"></text-structure>
-<text-structure title="2." content=" Visualize o gráfico: a função será automaticamente desenhada no plano cartesiano do jogo."></text-structure>
-<text-structure title="3." content="Desafie-se: tente ajustar a função para que a linha passe exatamente pelos espaços entre os canos, sem tocá-los."></text-structure>
+<text-structure title="2." content=" Desvie dos obstáculos: a função será automaticamente desenhada no plano cartesiano do jogo. Tente escrever uma função que faça a linha passar entre os canos sem tocá-los. Você pode usar →
+Funções básicas: x, x^2, 1/x
+Funções trigonométricas: sin(x), cos(x)
+Funções compostas: 2*sin(x) + x/3"></text-structure>
+<text-structure title="3." content="Dicas úteis: sin(x) e cos(x) são ótimos para criar curvas onduladas.
+
+abs(x) cria reflexos no eixo y.
+
+Funções do tipo x^2, x^3 formam parábolas e curvas suaves.
+
+."></text-structure>
 <text-structure title="4." content="Desafie-se: tente ajustar a função para que a linha passe exatamente pelos espaços entre os canos, sem tocá-los."></text-structure>
   </div>
   </div>
